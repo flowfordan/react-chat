72ca57eac0c4b16c956545748103b01003764b8f
flowfordan <flowfordan@gmail.com>
2022-04-05 14:32:37 +0300

    mini store

---
+++ src/store/usersStore.js
@@ -0,0 +1,10 @@
+const users = 
+    [
+        {id: 1, name: 'Boris'},
+        {id: 2, name: 'Joka'},
+        {id: 3, name: 'Boka'},
+        {id: 4, name: 'Pupa'},
+    ]
+
+
+export default users
