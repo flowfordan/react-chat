6af746c060bf849651ab22c0e9d74f612aa366b4
drawinger <sarut@bk.ru>
2022-04-05 13:20:06 +0300

    for newfix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
           Zdarova
           <h1>ZDAROVA</h1>
-          <h2>ZDAROVA</h2>
+          <h2>ZDAROVA2</h2>
+          <h2>ZDAROVA3</h2>
       </header>
       <div>
         Privet
